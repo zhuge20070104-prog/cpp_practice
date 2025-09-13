197e09b0b7bcdd11499031da226ac46bab947074
root <root@a1u1g-mil-0025.ipp2u1.colossus.nvidia.com>
2025-09-13 04:40:11 +0000

    add two examples

---
+++ cpp_beginer_to_advanced/148_declaring_and_using_lambda/main.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    []() {
+        std::cout << "Hello!\n";
+    };
+
+    // Lambda function without name, called instantly without name
+    []() {
+        std::cout << "Hello!\n";
+    }();
+
+    // Parameter list is not needed when parameters are not passed
+    auto print_hello {
+        [] {
+            std::cout << "Hello!\n";
+        }
+    };
+
+    // Calling a named lambda function
+    print_hello();
+
+
+    // With parameters, arguments passed at the direct call
+    [](int a, int b) {
+        std::cout << a + b << '\n';
+    }(2, 10);
+
+
+    // return value from lambda stored in double_sum then called with arguments
+    auto double_sum {
+        [](double a, double b) {
+            return a + b;
+        }
+    };
+
+    std::cout << double_sum(2.1, 2.2) << '\n';
+    std::cout << double_sum(3.2, 4.8) << '\n';
+
+    // lambda expression in a print statement
+    std::cout << [](double a, double b) { return a + b;}(22, 1) << '\n';
+
+    // Explictly specifying the return type of the lambda, implict conversion 
+    // from double to int type
+    auto force_int {
+        [](double a, double b) -> int {
+            return a + b;
+        }
+    };
+
+    std::cout << force_int(2.2, 2.2) << '\n';
+    std::cout << sizeof(force_int(2.2, 2.2)) << '\n';
+
+    return EXIT_SUCCESS;
+}
---
+++ cpp_beginer_to_advanced/149_captures_list/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    int x {1};
+    int y {1};
+
+    // Capturing x and y by value, both are copies inside the lambda
+    auto capture_list {
+        [x, y]() {
+            std::cout << "Inner x + y : " << x + y << '\n';
+        }
+    };
+
+    for(std::size_t i=0; i<5; ++i) {
+        capture_list();
+        std::cout << "Outer x + y: " << x + y << '\n';
+        ++x;
+        ++y;
+    }
+
+    std::cout << '\n';
+
+    auto capture_list_ref {
+        [&x, &y]() {
+            std::cout << "Inner x + y : " << x + y << '\n';
+        }
+    };
+
+    for(std::size_t i=0; i<5; ++i) {
+        capture_list_ref();
+        std::cout << "Outer x + y: " << x + y << '\n';
+        ++x;
+        ++y;
+    }
+
+    return EXIT_SUCCESS;
+}
