4a2225703954e2e03e166655eb1aaf94024e494a
Fredric2010 <fredric2010@outlook.com>
2025-09-16 13:08:11 +0000

    add several examples for classes

---
+++ cpp_beginer_to_advanced/181_arrow_pointer_call_notation/main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+const double PI {3.14};
+
+class Cylinder {
+public:
+    Cylinder() = default;
+
+    Cylinder(double base_radius, double height) {
+        m_base_radius = base_radius;
+        m_height = height;
+    }
+
+    double get_radius() {
+        return m_base_radius;
+    }
+
+    double get_height() {
+        return m_height;
+    }
+
+    void set_radius(double base_radius) {
+        m_base_radius = base_radius;
+    }
+
+    void set_height(double height) {
+        m_height = height;
+    }
+
+    double volume() {
+        return PI * m_base_radius * m_base_radius * m_height;
+    }
+
+private:
+    double m_base_radius {};
+    double m_height {};
+};
+
+
+int main(int argc, char* argv[]) {
+   
+    Cylinder c1 {5, 10};
+    std::cout << "Volume of c1: " << c1.volume() << std::endl;
+
+    Cylinder* p_c2 {new Cylinder(2, 4)};
+    std::cout << "Volume of c2: " << (*p_c2).volume() << std::endl;
+
+    // better way, arrow pointer operator
+    std::cout << "Volume of c2: " << p_c2->volume() << std::endl;
+
+    // Release the memory 
+    delete p_c2;
+
+    // Pointer to stack object
+    Cylinder* p_c3 {&c1};
+    std::cout << "Volume of c3: " << p_c3->volume() << std::endl;
+    
+    return EXIT_SUCCESS;
+}
+
---
+++ cpp_beginer_to_advanced/182_destructors/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string_view>
+
+
+class Dog {
+
+public:
+    Dog() = default;
+    Dog(std::string_view name, std::string_view breed, int age);
+    ~Dog();
+private:
+    std::string m_name;
+    std::string m_breed;
+    int* m_p_age {nullptr};
+};
+
+Dog::Dog(std::string_view name, std::string_view breed, int age) {
+    m_name = name;
+    m_breed = breed;
+    m_p_age = new int(age);
+}
+
+Dog::~Dog() {
+    delete m_p_age;
+    std::cout << m_name << " has been killed!\n";
+}
+
+void dog_destroyed() {
+    Dog test {"test", "test", 0};
+
+    Dog* p_test {new Dog("p_test", "test", 0)};
+    delete p_test;
+}
+
+int main(int argc, char* argv[]) {
+   
+    Dog d1 {"Tom", "Desi", 2};
+    Dog d2 {"Tom2", "Desi2", 4};
+
+    dog_destroyed();
+
+    std::cout << "main() died!\n";
+    return EXIT_SUCCESS;
+}
+
