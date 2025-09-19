daae7508b295f1fa12b372d8bd28dfc132d6cf2d
Fredric2010 <fredric2010@outlook.com>
2025-09-19 22:19:14 +0000

    add several examples

---
+++ cpp_beginer_to_advanced/196_uniform_initialization_for_aggragated/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+
+/*
+ * we can initialize any object in C++ using () or {}
+ *
+ * {} - doesn't allow narrowing conversions
+ * () - allows   
+ */
+ 
+class Point {
+private: 
+    double m_x {};
+    double m_y {};
+
+public:
+    Point(double x, double y): m_x(x), m_y(y) {}
+
+    std::string to_string() const {
+        return "Point(" + std::to_string(m_x) + ", " + std::to_string(m_y) + ")";
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Point p1 (5.9, 6.7);
+    std::cout << p1.to_string() << std::endl;
+
+    Point p2 {5.9, 6.7};
+    std::cout << p2.to_string() << std::endl;
+
+    int scores [] (1, 9, 8, 3);
+    for(auto const& score: scores) {
+        std::cout << score << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
+
