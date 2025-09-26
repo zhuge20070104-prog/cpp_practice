99c04f10b8dacfcb68285e6ecc8f7363f9880663
zhuge20070104-prog <Fredric2010@outlook.com>
2025-09-26 20:19:20 +0800

    add shared ptr array

---
+++ cpp_beginer_to_advanced/234_shared_ptrs_with_arrays/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <memory>
+#include "dog.h"
+
+
+int main(int argc, char* argv[]) {
+   
+    std::shared_ptr<int[]> shared_int_array {new int[]{1, 2, 3, 4, 5}};
+    std::shared_ptr<Dog[]> shared_dog_array {new Dog[]{Dog{"Dan"}, Dog{"Jude"}}};
+    
+    for(std::size_t i=0; i<5; ++i) {
+        std::cout << shared_int_array[i] << ' ';
+    }
+
+    std::cout << '\n';
+
+    for(std::size_t i=0; i<2; ++i) {
+        std::cout << shared_dog_array[i].get_name() << ' ';
+    }
+
+    std::cout << '\n';
+    return EXIT_SUCCESS;
+}
+
