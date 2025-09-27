3e80b07d02e42dae100f75b0ebf8d74b65bf7cf6
zhuge20070104-prog <Fredric2010@outlook.com>
2025-09-27 11:56:17 +0800

    fx

--- cpp_beginer_to_advanced/238_implicit_conversion_with_overriden_binary_ops/car.cpp
+++
@@ -1,8 +0,0 @@
-#include "car.h"
-
-Car::Car(const std::string& color, double speed):
-    m_color{color}, m_speed{speed} {
-
-}
-
-Car::~Car() {}
--- cpp_beginer_to_advanced/238_implicit_conversion_with_overriden_binary_ops/number.h
+++ cpp_beginer_to_advanced/238_implicit_conversion_with_overriden_binary_ops/number.h
@@ -1,7 +1,6 @@
 #ifndef _FREDRIC_NUMBER_H_
 #define _FREDRIC_NUMBER_H_
 
-#include "point.h"
 
 class Number {
     friend std::ostream& operator<<(std::ostream& out, const Number& number); 
@@ -20,26 +19,7 @@ public:
     }
 
     ~Number() {}
-    // Number operator+(const Number& right) {
-    //     return {m_wrappered_int + right.m_wrappered_int};
-    // }
-
-    // Number operator-(const Number& right) {
-    //     return {m_wrappered_int - right.m_wrappered_int};
-    // }
-
-    // operator double() const {
-    //     std::cout << "Using the member double() to convert\n";
-    //     return static_cast<double>(m_wrappered_int);
-    // }
-
-    operator Point() const {
-        std::cout << "Using the member Point() to convert\n";
-        return Point{static_cast<double>(m_wrappered_int),
-             static_cast<double>(m_wrappered_int),
-             m_wrappered_int};
-    }
-
+   
 private:
     int m_wrappered_int{};
 };
