23d86e06fece768cb7b58b849f926a39978f7824
zhuge20070104-prog <Fredric2010@outlook.com>
2025-09-27 11:49:53 +0800

    fx

--- cpp_beginer_to_advanced/238_implicit_conversion_with_overriden_binary_ops/main.cpp
+++ cpp_beginer_to_advanced/238_implicit_conversion_with_overriden_binary_ops/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char* argv[]) {
     
     Number n1 {87};
     std::cout << "n1: " << n1 << '\n';
-    std::cout << "n1 + 3.0: " << n1 + 3.0 << '\n';
-    std::cout << "3.0 + n1: " << 3.0 - n1 << '\n';
-    std::cout << "1.0 - n1: " << 1.0 - n1 << '\n';
+    std::cout << "n1 + 3: " << n1 + 3 << '\n';
+    std::cout << "3 + n1: " << 3 - n1 << '\n';
+    std::cout << "1 - n1: " << 1 - n1 << '\n';
     return EXIT_SUCCESS;
 }
