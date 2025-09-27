5a66d36f2f60eacd50363210e01bb0a69241bde0
zhuge20070104-prog <Fredric2010@outlook.com>
2025-09-27 10:04:01 +0800

    new feat

--- cpp_beginer_to_advanced/237_custom_type_conversions/number.h
+++ cpp_beginer_to_advanced/237_custom_type_conversions/number.h
@@ -5,6 +5,35 @@
 
 class Number {
     friend std::ostream& operator<<(std::ostream& out, const Number& number); 
+    friend Number operator+(const Number& left, const Number& right);
+    friend Number operator-(const Number& left, const Number& right);
+    friend Number operator*(const Number& left, const Number& right);
+    friend Number operator/(const Number& left, const Number& right);
+    friend Number operator%(const Number& left, const Number& right);
+
+public:
+    Number() = default;
+    Number(int value);
+
+    int get_wrappered_int() const {
+        return m_wrappered_int;
+    }
+
+    ~Number() {}
+    // Number operator+(const Number& right) {
+    //     return {m_wrappered_int + right.m_wrappered_int};
+    // }
+    operator double() const {
+        std::cout << "Using the member double() to convert\n";
+        return static_cast<double>(m_wrappered_int);
+    }
+
+    operator Point() const {
+        std::cout << "Using the member Point() to convert\n";
+        return Point{m_wrappered_int, m_wrappered_int, m_wrappered_int};
+    }
+private:
+    int m_wrappered_int{};
 };
 
 #endif
---
+++ cpp_beginer_to_advanced/237_custom_type_conversions/point.h
@@ -0,0 +1,108 @@
+#ifndef _FREDRIC_POINT_H_
+#define _FREDRIC_POINT_H_
+
+class Number;
+class Car;
+
+#include <iostream>
+#include <cassert>
+
+
+class Point {
+    Point() = default;
+
+    Point(double x, double y, int data);
+    Point(const Number& num);
+    void print_info() const {
+        std::cout << m_x << ", " << m_y << '\n';
+    }
+
+    ~Point();
+
+    double& operator[](std::size_t index) {
+        assert((index==0) || (index==1));
+        return (index==0) ? m_x: m_y;
+    }
+
+    Point operator+(const Point& right) const {
+        std::cout << "Member operator + called!\n";
+        return Point{m_x + right.m_x, m_y + right.m_y};
+    }
+
+    // << as a member function
+    // left operand will be the point class and right will be the ostream class - awkward
+    std::ostream& operator<<(std::ostream& os) const {
+        os << "Point: [" << m_x << ',' << m_y << ']' << '\n';
+        std::cout << "Member operator << called\n";
+        return os;
+    }
+
+    Point(const Point& right) {
+        if(this!=&right) {
+            delete point_data;
+            point_data = new int {*(right.point_data)};
+            m_x = right.m_x;
+            m_y = right.m_y;
+        }
+        std::cout << "Copy constructor called\n";
+    }
+
+
+    Point operator=(const Point& right) {
+        if(this!=&right) {
+            delete point_data;
+            m_x = right.m_x;
+            m_y = right.m_y;
+            point_data = new int{*(right.point_data)};
+        }
+        std::cout << "Copy assignment for Point called\n";
+        return *this;
+    }
+
+    friend Point operator+(const Point& left, const Point& right);
+    friend Point operator-(const Point& left, const Point& right);
+    friend Point operator*(const Point& left, const Point& right);
+    friend Point operator/(const Point& left, const Point& right);
+
+    friend std::ostream& operator<<(std::ostream& out, const Point& p);
+    firend std::istream& operator>>(std::istream& is, Point& p);
+
+    void operator++() {
+        ++m_x;
+        ++m_y;
+    }
+
+    // int - necessary to differentiate the postfix from prefix
+    // postfix ++ as a member
+    // returned type can be set according to the need
+    Point operator++(int) {
+        Point local_point{*this};
+        ++(*this);
+        return local_point;
+    }
+
+    void operator--() {
+        --m_x;
+        --m_y;
+    }
+
+    Point operator--(int) {
+        Point local_point{*this};
+        --(*this);
+        return local_point;
+    }
+
+    // copy a Car object to Point
+    // void - chained assignments are not supported
+    void operator=(const Car& right_car);
+    void operator=(const Number& n);
+
+private:
+    double m_x;
+    double m_y;
+    int* point_data{nullptr};
+};
+
+// TODO: finish Point's inline functions
+
+#endif
