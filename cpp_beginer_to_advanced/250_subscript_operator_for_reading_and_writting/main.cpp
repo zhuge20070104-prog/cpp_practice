e7b8f48b33110c86d2e6c245966c5afc4fa40535
Fredric <Fredric2010@outlook.com>
2025-09-29 05:50:35 +0800

    add comment

--- cpp_beginer_to_advanced/250_subscript_operator_for_reading_and_writting/main.cpp
+++ cpp_beginer_to_advanced/250_subscript_operator_for_reading_and_writting/main.cpp
@@ -8,10 +8,11 @@ int main(int argc, char* argv[]) {
     Point p1 {9.8, 5.1, 0};
     std::cout << "p1[0]: " << p1[0] << " p1[1]: " << p1[1] << '\n';
 
+    // left syntax returns a reference to the m_x and m_y which can be used to modify the data
     p1[0] = 9.9;
     p1[1] = 6.6;
 
     std::cout << "p1[0]: " << p1[0] << " p1[1]: " << p1[1] << '\n';
-    
+
     return EXIT_SUCCESS;
 }
