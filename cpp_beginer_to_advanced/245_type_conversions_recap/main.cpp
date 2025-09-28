090da0ca88d5c9b1e574d9a2f4d77b3d8b8eb15f
Fredric <Fredric2010@outlook.com>
2025-09-28 13:28:57 +0800

    add several samples

---
+++ cpp_beginer_to_advanced/245_type_conversions_recap/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "point.h"
+#include "car.h"
+
+void do_something(const Point& p) {
+    std::cout << "from_func: " << p << '\n';
+}
+
+int main(int argc, char* argv[]) {
+    Point p1 {0, 0, 0};
+    Number n1 {2};
+
+    p1 = n1;
+
+    do_something(n1);
+    return EXIT_SUCCESS;
+}
