0895ab576117c2da0297c65801e9de8a4926f482
Fredric <Fredric2010@outlook.com>
2025-09-29 08:12:36 +0800

    stream insertion operation operator

---
+++ cpp_beginer_to_advanced/252_stream_insertion_operation_operator/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "point.h"
+#include "car.h"
+#include "number.h"
+
+
+int main(int argc, char* argv[]) {
+    Point p1 {3.8, 9.2, 0};
+    Point p2 {1.0, 6.7, 0};
+
+    //  awkward, when << is a member of the Point class
+    p1 << std::cout;
+    
+    // intuitive, operator<< as friend of Point, chaining of statements
+    std::cout << "p1: " << p1 << ', ' << p2 << '\n';
+
+    return EXIT_SUCCESS;
+}
