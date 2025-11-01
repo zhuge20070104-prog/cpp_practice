5e018d4d4330ff919498c5d70aafea0ff4ebc62a
fredricz <fredricz@nvidia.com>
2025-11-02 07:35:43 +0800

    add two word count examples

---
+++ taming_big_data/lesson_20/word_count_better_sorted.py
@@ -0,0 +1,20 @@
+import re
+from pyspark import SparkConf, SparkContext
+
+def normalizeWords(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+input = sc.textFile("file:///mnt/c/code/cpp_practice/taming_big_data/datasets/Book")
+words = input.flatMap(normalizeWords)
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey()
+results = wordCountsSorted.collect()
+
+for result in results:
+    count = str(result[0])
+    word = result[1].encode('ascii', 'ignore')
+    if word:
+        print(word.decode() + ":\t\t" + str(count))
