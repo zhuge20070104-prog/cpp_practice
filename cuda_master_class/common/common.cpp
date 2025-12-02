294207d34e91a4c6e17f30bea5cbcf6041c7deb1
fredric <you@example.com>
2025-12-02 21:01:06 +0800

    add compare results func
    Signed-off-by: fredric <you@example.com>

--- cuda_master_class/common/common.cpp
+++ cuda_master_class/common/common.cpp
@@ -83,6 +83,20 @@ void compare_results(int gpu_result, int cpu_result)
 	printf("GPU and CPU results are different \n");
 }
 
+void compare_results(float gpu_result, float cpu_result) {
+	printf("GPU result : %f , CPU result : %f \n",
+		gpu_result, cpu_result);
+
+	if(fabs(gpu_result - cpu_result) < 1e-6) {
+		printf("GPU and CPU results are same \n");
+		return;
+	}
+
+	printf("GPU and CPU results are different \n");
+	printf("GPU result : %f , CPU result : %f \n",
+		gpu_result, cpu_result);
+	printf("Difference : %f \n", fabs(gpu_result - cpu_result));
+}
 
 void sum_array_cpu(float* a, float* b, float* c, int size) {
 	for(int i=0; i<size; i++) {
